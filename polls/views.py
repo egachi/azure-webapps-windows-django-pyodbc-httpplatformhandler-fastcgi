e8cfedecc58cd2222251d4ebff5ca0d88ea26706
mredison <egachi@live.com>
2018-05-22 10:28:35 -0500

    Adding all files

---
+++ polls/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+import pyodbc 
+
+def index(request):
+    server = 'tcp:edisga-server.database.windows.net' 
+    database = 'edisga-sql' 
+    username = 'mredison' 
+    password = 'Pa$$w0rd123456' 
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    #Sample select query
+    cursor.execute("SELECT * FROM SalesLT.Product") 
+    row = cursor.fetchone()
+    print(row)          # access by column index (zero-based)
+    return HttpResponse(row)
